2117dbddc6e57fcef4736eeb4eaf738125922983
MilyangParkJaeHun <jh9277@naver.com>
2022-01-20 00:05:15 +0900

    Add documents

--- ObjectTracking/SORT/sort.py
+++ ObjectTracking/SORT/sort.py
@@ -19,13 +19,13 @@ from typing import List
 np.random.seed(0)
 
 def x_to_bbox(x):
-    """[summary]
+    """ 칼만 필터 상태 벡터 값을 bounding box 값으로 변환
 
     Args:
-        x ([type]): [description]
+        x (u, v, s, r, u', v', s'): 칼만 필터 상태 벡터
 
     Returns:
-        [type]: [description]
+        bbox: xmin, ymin, xmax, ymax 로 이루어진 bounding box 값
     """
     u, v, s, r = x[:4]
     w = np.sqrt(s * r)
@@ -38,13 +38,13 @@ def x_to_bbox(x):
     return np.array([xmin, ymin, xmax, ymax])
 
 def bbox_to_z(bbox):
-    """[summary]
+    """ bounding box 값을 측정 벡터 값으로 변환
 
     Args:
-        bbox ([type]): [description]
+        bbox (xmin, ymin, xmax, ymax): bounding box 정보
 
     Returns:
-        [type]: [description]
+        [u, v, s, r]: x 중심 좌표, y 중심 좌표, 넓이, 비율 정보
     """
     xmin, ymin, xmax, ymax, _ = bbox
     w = xmax - xmin
@@ -55,27 +55,19 @@ def bbox_to_z(bbox):
     r = w / float(h)
     return np.array([u, v, s, r]).reshape((4, 1))
 
-class BoxTracker(object):
-    """[summary]
-
-    Args:
-        object ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    count = 0
+class BoxTracker(object): # 칼만 필터 기반 단일 객체 추적기
+    count = 0 # 단일 객체 추적기의 id 를 할당해주기 위한 변수
     def __init__(self, bbox):
-        """[summary]
+        """ 단일 객체 추적기 생성자
 
         Args:
-            bbox ([type]): [description]
+            bbox (xmin, ymin, xmax, ymax): 객체 추적을 시작할 bbox 좌표값
         """
         self.id = BoxTracker.count
         BoxTracker.count += 1
 
         # x = [u, v, s, r, u', v', s']
-        self.kf = KalmanFilter(dim_x=7, dim_z=4)
+        self.kf = KalmanFilter(dim_x=7, dim_z=4) # 객체 위치 예측을 수행할 칼만필터 instance 생성
         self.kf.F = np.array([[1,0,0,0,1,0,0],
                            [0,1,0,0,0,1,0],
                            [0,0,1,0,0,0,1],
@@ -87,28 +79,28 @@ class BoxTracker(object):
                            [0,1,0,0,0,0,0],
                            [0,0,1,0,0,0,0],
                            [0,0,0,1,0,0,0]])
-        self.kf.P *= 10.
+        self.kf.P *= 10. # 초기 상태의 예측 불확실성을 고려
         self.kf.P[4:,4:] *= 100. # 초기 속도 예측에 대한 높은 불확실성을 반영
-        self.kf.Q[4:,4:] *= 0.01
-        self.kf.Q[-1,-1] *= 0.01
-        self.kf.R[2:,2:] *= 10.
+        self.kf.Q[4:,4:] *= 0.01 # 속도에 대해 완만한 추정값을 얻기 위해 작게 조정
+        self.kf.Q[-1,-1] *= 0.01 # 넓이의 속도는 높이와 너비의 속도가 곱해져 반영되므로 0.01 을 다시 반영
+        self.kf.R[2:,2:] *= 10. # 너비와 비율이 측정값의 영향을 덜 받고 완만하게 추정하도록 R 값을 크게 반영
 
-        self.kf.x[:4] = bbox_to_z(bbox)
+        self.kf.x[:4] = bbox_to_z(bbox) # 첫 bbox 를 측정 벡터로 변환하여 칼만필터 상태 벡터 초기화에 사용
 
-        self.age = 0
-        self.hits = 0
-        self.time_since_update = 0
+        self.age = 0 # 추적을 유지해온 횟수
+        self.hits = 0 # 매칭에 성공한 횟수
+        self.time_since_update = 0 # 마지막 매칭에 성공한 이후부터 지나온 횟수
 
     def predict(self):
-        """[summary]
+        """ 추적하던 객체의 다음 위치 예측
 
         Returns:
-            [type]: [description]
+            bbox: 추적하던 객체의 다음 시점의 bbox 예측값
         """
-        if((self.kf.x[6]+self.kf.x[2])<=0):
+        if((self.kf.x[6]+self.kf.x[2])<=0): # 예측될 bbox 의 넓이가 0 보다 작다면 0 으로 설정
             self.kf.x[6] *= 0.0
-        self.kf.predict()
-        next_bbox = x_to_bbox(self.kf.x)
+        self.kf.predict() # 예측 수행
+        next_bbox = x_to_bbox(self.kf.x) 
 
         self.age += 1
         if self.time_since_update > 0:
@@ -118,28 +110,28 @@ class BoxTracker(object):
         return next_bbox
 
     def update(self, bbox):
-        """[summary]
+        """ 측정값을 사용하여 칼만 필터 업데이트
 
         Args:
-            bbox ([type]): [description]
+            bbox (xmin, ymin, xmax, ymax): 객체 검출기로부터의 bbox 측정값
         """
-        self.kf.update(bbox_to_z(bbox))
+        self.kf.update(bbox_to_z(bbox)) # 업데이트 수행
         self.time_since_update = 0
         self.hits += 1
 
     def get_state(self):
-        """[summary]
+        """ 추적하는 객체의 bbox 값 출력
 
         Returns:
-            [type]: [description]
+            bbox: xmin, ymin, xmax, ymax
         """
         return x_to_bbox(self.kf.x)
     
     def get_scale(self):
-        """[summary]
+        """ 추적하는 객체의 bbox 넓이 출력
 
         Returns:
-            [type]: [description]
+            float: 추적하는 객체의 bbox 넓이
         """
         return self.kf.x[2]
 
@@ -177,11 +169,11 @@ class Sort(object): # 다중 객체 추적을 수행하는 Class
             """ 비정상적 예측 결과 필터링
 
             Args:
-                scale ([float]): 예측된 bbox 의 넓이
-                bbox ([xmin, ymin, xmax, ymax]): 예측된 bbox 정보
+                scale (float): 예측된 bbox 의 넓이
+                bbox (xmin, ymin, xmax, ymax): 예측된 bbox 정보
 
             Returns:
-                [bool]: 비정상적이면 True, 정상적이면 False
+                bool: 비정상적이면 True, 정상적이면 False
             """
             return scale <= 0 or np.any(np.isnan(bbox))
 
@@ -228,22 +220,23 @@ class Sort(object): # 다중 객체 추적을 수행하는 Class
         return np.array(trk_res, dtype=object)
 
     def match_dets_with_trks(self, dets, preds):
-        """[summary]
+        """ 객체 검출과 예측값의 최적 매칭을 
 
         Args:
-            dets ([type]): [description]
-            preds ([type]): [description]
+            dets ([xmin, ymin, xmax, ymax]): 객체 검출 bbox
+            preds ([xmin, ymin, xmax, ymax]): 객체의 다음 위치 예측 bbox
         """
         def cal_iou(bb_a, bb_b): # iou score 계산
-            """[summary]
+            """ bbox batch A 와 bbox batch B 사이의 iou score matrix 계산
 
             Args:
-                bb_a ([type]): [description]
-                bb_b ([type]): [description]
+                bb_a ([bbox]): 하나 이상의 bbox 를 포함하는 리스트
+                bb_b ([bbox]): 하나 이상의 bbox 를 포함하는 리스트
 
             Returns:
-                [type]: [description]
+                2DMatrix: A 와 B 간의 iou score 값으로 이루어진 2차원 매트릭스
             """
+            # input(dets, preds) 의 순서에 맞춰 ouput 이 출력되도록 확장 차원 순서 조정
             bb_b = np.expand_dims(bb_b, 0)
             bb_a = np.expand_dims(bb_a, 1)
 
@@ -258,35 +251,35 @@ class Sort(object): # 다중 객체 추적을 수행하는 Class
             union = (bb_a[..., 2] - bb_a[..., 0]) * (bb_a[..., 3] - bb_a[..., 1]) + \
                     (bb_b[..., 2] - bb_b[..., 0]) * (bb_b[..., 3] - bb_b[..., 1]) - overlap
             
-            iou_matrix = overlap / union
+            iou_matrix = overlap / union # iou score 계산
 
             return iou_matrix
 
-        iou_matrix = cal_iou(dets, preds)
+        iou_matrix = cal_iou(dets, preds) # dets 와 preds 의 모든 조합에 대한 iou score 계산
 
         matches = []
         unmatched_dets = []
         unmatched_trks = []
         if min(iou_matrix.shape) > 0: # 매칭의 가능성이 있다면 iou score 기반으로 최적 매칭 서치
-            det_indices, pred_indices = linear_sum_assignment(-iou_matrix) # 헝가리안 알고리즘
-            matched_indices = np.array(list(zip(det_indices, pred_indices)))
+            det_indices, pred_indices = linear_sum_assignment(-iou_matrix) # 헝가리안 알고리즘(이분 그래프에서 최소 가중치 매칭을 찾는 알고리즘이므로 최대 가중치 매칭을 위해 iou_matrix 의 부호를 반전)
+            matched_indices = np.array(list(zip(det_indices, pred_indices))) # ((det_index, pre_index)) 꼴로 매칭 결과 변환
             
             for det_i, pred_i in matched_indices:
-                if(iou_matrix[det_i][pred_i] < self.iou_threshold):
+                if(iou_matrix[det_i][pred_i] < self.iou_threshold): # 매칭된 결과의 iou score 가 iou threshold 보다 낮다면 매칭 실패로 간주
                     unmatched_dets.append(det_i)
                     unmatched_trks.append(pred_i)
                 else:
-                    matches.append([det_i, pred_i])
+                    matches.append([det_i, pred_i]) # iou threshold 보다 크다면 매칭 성공으로 간주
 
-            for i in range(len(dets)):
+            for i in range(len(dets)): # 매칭에 실패한 객체 검출 결과의 인덱스 저장
                 if i not in matched_indices[:, 0]:
                     unmatched_dets.append(i)
 
-            for i in range(len(preds)):
+            for i in range(len(preds)): # 매칭에 실패한 추적기의 인덱스 저장
                 if i not in matched_indices[:, 1]:
                     unmatched_trks.append(i)
             
-        else:
+        else: # 모든 매칭에 실패했으므로 실패한 객체 검출 결과와 추적기의 인덱스 저장
             unmatched_dets = list(range(len(dets)))
             unmatched_trks = list(range(len(preds)))
         
