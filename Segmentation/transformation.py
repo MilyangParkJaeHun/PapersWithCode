023fce939acd1091b07f0b9d31def2c90a7d1089
MilyangParkJaeHun <jh9277@naver.com>
2022-12-28 10:15:02 +0000

    Converted Rescale function to using torchvision function
    Signed-off-by: MilyangParkJaeHun <jh9277@naver.com>

--- Segmentation/transformation.py
+++ Segmentation/transformation.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 from skimage import io, transform
 from torchvision.transforms import functional as F
-from torchvision import transforms
+from torchvision.transforms import InterpolationMode
 
 
 class ConvertLabel(object):
@@ -16,32 +16,6 @@ class ConvertLabel(object):
         return {'image': image, 'label': lbl, 'name': name}
 
 
-class Rescale(object):
-
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        self.output_size = output_size
-
-    def __call__(self, sample):
-        image, label, name = sample['image'], sample['label'], sample['name']
-
-        h, w, c = image.shape
-        if isinstance(self.output_size, int):
-            if h > w:
-                new_h, new_w = self.output_size * h / w, self.output_size
-            else:
-                new_h, new_w = self.output_size, self.output_size * w / h
-        else:
-            new_h, new_w = self.output_size
-
-        new_h, new_w = int(new_h), int(new_w)
-
-        img = cv2.resize(image, (new_h, new_w), cv2.INTER_LINEAR)
-        lbl = cv2.resize(label, (new_h, new_w), cv2.INTER_NEAREST)
-
-        return {'image': img, 'label': lbl, 'name': name}
-
-
 class ToTensor(object):
 
     def __call__(self, sample):
@@ -52,6 +26,34 @@ class ToTensor(object):
         return {'image': torch.from_numpy(img).float(),
                 'label': torch.from_numpy(label).long(),
                 'name': name}
+
+
+class Rescale(object):
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label, name = sample['image'], sample['label'], sample['name']
+
+        h, w, _ = image.shape
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+
+        img = F.resize(image, (new_h, new_w))
+        lbl = torch.unsqueeze(label, dim=0)
+        lbl = F.resize(lbl, (new_h, new_w), interpolation=InterpolationMode.NEAREST)
+        lbl = torch.squeeze(lbl)
+
+        return {'image': img, 'label': lbl, 'name': name}
 
 
 class Normalize(object):
