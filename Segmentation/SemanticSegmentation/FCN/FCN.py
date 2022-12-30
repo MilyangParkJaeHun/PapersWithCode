e45086166d926496e6e5f15feb91682c3c55668a
MilyangParkJaeHun <jh9277@naver.com>
2022-12-30 07:25:05 +0000

    Remove backbone network implementation in FCN.py
    Signed-off-by: MilyangParkJaeHun <jh9277@naver.com>

--- Segmentation/SemanticSegmentation/FCN/FCN.py
+++ Segmentation/SemanticSegmentation/FCN/FCN.py
@@ -1,14 +1,16 @@
-import functools
+import os
 import sys
-sys.path.append('/home/park/workspace/PapersWithCode/Segmentation')
+sys.path.append(os.environ['WORKSPACE_PATH'])
 
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
-from data_utils import CustomDataset
-from transformation import *
+from BackboneNetwork.VGGNet.net import VGGNet
+from Segmentation.data_utils import CustomDataset
+from Segmentation.transformation import *
+
 
 class FCN8s(nn.Module):
 
@@ -56,66 +58,6 @@ class FCN8s(nn.Module):
 
         return output
 
-class VGGNet(nn.Module):
-    def __init__(self, mode, in_channels):
-        super().__init__()
-
-        num_classes = 14
-        self.in_channels = in_channels
-        mode = mode.lower()
-        self.cfg = self.make_cfgs()[mode]
-        self.features, self.output_point = self.make_layers()
-
-    def make_layers(self, batch_norm = False):
-        layers = []
-        output_point = {}
-
-        in_channels = self.in_channels
-        n_layer = 0
-        for block_index, value_list in self.cfg.items():
-            for value in value_list:
-                if value == 'M':
-                    layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-                    n_layer += 1
-                else:
-                    value = int(value)
-                    conv2d = nn.Conv2d(in_channels, value, kernel_size=3, padding=1)
-                    if batch_norm:
-                        layers += [conv2d, nn.BatchNorm2d(value), nn.ReLU(inplace=True)]
-                        n_layer += 3
-                    else:
-                        layers += [conv2d, nn.ReLU(inplace=True)]
-                        n_layer += 2
-                    in_channels = value
-            output_point[block_index] = n_layer - 1
-
-        return nn.Sequential(*layers), output_point
-
-
-    def make_cfgs(self):
-        cfgs = {}
-        cfgs['vgg16'] = {
-            1 : [64, 64, "M"],
-            2 : [128, 128, "M"],
-            3 : [256, 256, 256, "M"],
-            4 : [512, 512, 512, "M"],
-            5 : [512, 512, 512, "M"]}
-        return cfgs
-
-
-    def forward(self, x):
-        output = {}
-        block_index = 1
-        n_layers = len(self.features)
-        for index in range(n_layers):
-            x = self.features[index](x)
-
-            if index == self.output_point[block_index]:
-                output["x%d"%(block_index)] = x
-                block_index += 1
-        return output
-
-
 def test_model(model, data_loader):
     phase = 'valid'
 
@@ -132,8 +74,6 @@ def test_model(model, data_loader):
 
 
 if __name__ == '__main__':
-    vgg16 = VGGNet('vgg16', 3)
-
     data_params = dict()
     data_params['root_dir'] = '/home/park/DATA/2022_11_23/trails'
     data_params['image_size'] = (128, 256)
@@ -165,10 +105,6 @@ if __name__ == '__main__':
                             # pin_memory_device=device,
                             prefetch_factor=2)
 
-    vgg = VGGNet('vgg16', 3).to(device)
+    vgg = VGGNet('vgg19', 3).to(device)
     fcn = FCN8s(vgg, 14).to(device)
     test_model(fcn, data_loader)
-
-    # print(vgg16)
-    # for param_tensor in fcn.state_dict():
-    #     print(param_tensor, "\t", fcn.state_dict()[param_tensor].size())
